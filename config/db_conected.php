e849a522b9eed58b286e17f240783c608ca1407c
Rawezh0894 <rawezh.soft94@gmail.com>
2025-07-12 14:26:54 +0300

    first commit

--- config/db_conected.php
+++ config/db_conected.php
@@ -4,9 +4,9 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 // Database connection settings
 $host = 'localhost';
-$db   = 'dana_concrete_db';
-$user = 'root'; // گەر ناوی بەکارهێنەر یان وشەی نهێنی جیاوازە، گۆڕی
-$pass = '';
+$db   = 'dana_concrete';
+$user = 'webuser'; // گەر ناوی بەکارهێنەر یان وشەی نهێنی جیاوازە، گۆڕی
+$pass = 'Rawezh.Jaza@0894';
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
