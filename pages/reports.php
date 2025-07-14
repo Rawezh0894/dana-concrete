f1a3c57423de797390d74f45ee2ca1cedad81a51
Rawezh0894 <rawezh.soft94@gmail.com>
2025-07-14 17:23:43 +0300

    Initial commit

--- pages/reports.php
+++ pages/reports.php
@@ -4,7 +4,8 @@
 <!DOCTYPE html>
 <html lang="ku" dir="rtl">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ڕاپۆرتی گشتی</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.rtl.min.css" rel="stylesheet">
     <link href="../assets/css/login.css" rel="stylesheet">
