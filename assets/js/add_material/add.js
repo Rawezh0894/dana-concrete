a7f5ad20485f9244583672e22eb1ee942b4819a1
Rawezh0894 <rawezh.soft94@gmail.com>
2025-07-27 10:41:58 +0300

    گۆڕانکاریە نوێیەکان

--- assets/js/add_material/add.js
+++ assets/js/add_material/add.js
@@ -4,28 +4,77 @@ $(function() {
     var $btn = $(this).find('button[type="submit"]');
     $btn.prop('disabled', true);
     var formData = $(this).serialize();
+    
+    console.log('Submitting form data:', formData);
+    
     $.post('../process/add_material/add.php', formData, function(res) {
-      if (res.trim() === 'success') {
-        Swal.fire({
-          icon: 'success',
-          title: 'سەرکەوتوو',
-          text: 'کاڵا بە سەرکەوتوویی زیادکرا!',
-          confirmButtonText: 'باشە'
-        }).then(() => {
-          $('#addMaterialModal').modal('hide');
-          if (typeof loadMaterials === 'function') loadMaterials();
-          $('#addMaterialForm')[0].reset();
-        });
-      } else {
-        Swal.fire({
-          icon: 'error',
-          title: 'هەڵە',
-          text: 'زیادکردن سەرکەوتوو نەبوو!',
-          confirmButtonText: 'باشە'
-        });
+      console.log('Server response:', res);
+      
+      try {
+        // Try to parse as JSON first
+        var jsonResponse = JSON.parse(res);
+        console.log('Parsed JSON response:', jsonResponse);
+        
+        if (jsonResponse.status === 'success') {
+          Swal.fire({
+            icon: 'success',
+            title: 'سەرکەوتوو',
+            text: jsonResponse.message || 'کاڵا بە سەرکەوتوویی زیادکرا!',
+            confirmButtonText: 'باشە'
+          }).then(() => {
+            $('#addMaterialModal').modal('hide');
+            if (typeof loadMaterials === 'function') loadMaterials();
+            $('#addMaterialForm')[0].reset();
+          });
+        } else {
+          Swal.fire({
+            icon: 'error',
+            title: 'هەڵە',
+            text: jsonResponse.message || 'زیادکردن سەرکەوتوو نەبوو!',
+            confirmButtonText: 'باشە'
+          });
+        }
+      } catch (e) {
+        // If not JSON, treat as plain text
+        console.log('Response is not JSON, treating as plain text');
+        
+        if (res.trim() === 'success') {
+          Swal.fire({
+            icon: 'success',
+            title: 'سەرکەوتوو',
+            text: 'کاڵا بە سەرکەوتوویی زیادکرا!',
+            confirmButtonText: 'باشە'
+          }).then(() => {
+            $('#addMaterialModal').modal('hide');
+            if (typeof loadMaterials === 'function') loadMaterials();
+            $('#addMaterialForm')[0].reset();
+          });
+        } else {
+          Swal.fire({
+            icon: 'error',
+            title: 'هەڵە',
+            text: 'زیادکردن سەرکەوتوو نەبوو! Response: ' + res,
+            confirmButtonText: 'باشە'
+          });
+        }
       }
+      
       $btn.prop('disabled', false);
-    }).fail(function() {
+    }).fail(function(xhr, status, error) {
+      console.error('AJAX Error:', {
+        status: status,
+        error: error,
+        responseText: xhr.responseText,
+        statusCode: xhr.status
+      });
+      
+      Swal.fire({
+        icon: 'error',
+        title: 'هەڵەی AJAX',
+        text: 'هەڵە لە پەیوەندی بە سێرڤەر: ' + error + ' (Status: ' + xhr.status + ')',
+        confirmButtonText: 'باشە'
+      });
+      
       $btn.prop('disabled', false);
     });
   });
