fb99b0ec15f9f9f25d780cf45288353e37849a10
Rawezh0894 <rawezh.soft94@gmail.com>
2025-07-30 08:32:54 +0300

    first commit

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -13,6 +13,7 @@ if (!isset($_SESSION['user_id'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>داشبۆرد</title>
+    <link rel="icon" type="image/x-icon" href="../assets/images/logo.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.rtl.min.css" rel="stylesheet">
     <link href="../assets/css/variables.css" rel="stylesheet">
     <link href="../assets/css/dashboard.css" rel="stylesheet">
