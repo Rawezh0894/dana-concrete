8f24929e92918eef1bd55a97dc8f5fe1ade00aee
Rawezh0894 <rawezh.soft94@gmail.com>
2025-08-04 11:33:40 +0300

    گۆڕانکاریە نوێیەکان

--- process/summery_concrete_receipts/get_informations.php
+++ process/summery_concrete_receipts/get_informations.php
@@ -89,7 +89,7 @@ try {
         $where_clause
         GROUP BY c.id, c.name, c.mobile1
         HAVING receipt_count > 0
-        ORDER BY total_meter DESC
+        ORDER BY receipt_count ASC
     ";
 
     $customer_summary_stmt = $pdo->prepare($customer_summary_query);
